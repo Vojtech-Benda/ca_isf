cfe60e72160bac490eb8212362ba52759f70be72
Vojtech-Benda <vojtech.benda@atlas.cz>
2023-09-06 17:48:20 +0200

    Renamed file

--- ca_iss_app/ca_iss_data_storage.py
+++ ca_iss_app/ca_iss_data_storage.py
@@ -1,21 +1,25 @@
 import SimpleITK as sitk
-from typing import Union
 from dataclasses import dataclass, field
-
+from PySide6.QtCore import QPoint
 
 @dataclass
 class CtData:
     ct_volume: sitk.Image = field(default_factory=sitk.Image)
-    ct_meta: dict[str, Union[tuple, str, float]] = field(default_factory=dict)
+    ct_meta: dict = field(default_factory=dict)
 
 
 @dataclass
 class XrayData:
     xray_image: sitk.Image = field(default_factory=sitk.Image)
-    xray_meta: dict[str, Union[str | float]] = field(default_factory=dict)
+    xray_meta: dict = field(default_factory=dict)
 
 
 @dataclass
 class DrrData:
     drr_image: sitk.Image = field(default_factory=sitk.Image)
-    drr_meta: dict[str, int] = field(default_factory=dict)
+    drr_meta: dict = field(default_factory=dict)
+
+
+@dataclass
+class PointList:
+    points: list[QPoint] = field(default_factory=list[QPoint])
