d96123acbbbca49eea250030f707459917b9962e
Vojtech-Benda <vojtech.benda@atlas.cz>
2024-04-21 14:10:58 +0200

    updated data storage

--- casif_app/casif_data_storage.py
+++ casif_app/casif_data_storage.py
@@ -3,12 +3,6 @@ from dataclasses import dataclass, field
 
 
 @dataclass
-class PreOpCtData:
-    preop_ct_volume: sitk.Image = field(default_factory=sitk.Image)
-    preop_ct_meta: dict = field(default_factory=dict)
-
-
-@dataclass
 class IntraOpCtData:
     intraop_ct_volume: sitk.Image = field(default_factory=sitk.Image)
     intraop_ct_meta: dict = field(default_factory=dict)
@@ -17,11 +11,11 @@ class IntraOpCtData:
 @dataclass
 class PreOpDrrData:
     preop_drr_image: sitk.Image = field(default_factory=sitk.Image)
-    preop_drr_exist_state: bool = False
+    exist_state: bool = False
 
 
 @dataclass
 class IntraOpDrrData:
     intraop_drr_image: sitk.Image = field(default_factory=sitk.Image)
-    intra_drr_exist_state: bool = False
+    exist_state: bool = False
 
